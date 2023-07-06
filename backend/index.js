028bc4fa2ae5a1c528d3a0a61712512b8b3b04d6
riyagupta2011 <riyagupta75751@gmail.com>
2023-07-07 00:59:00 +0530

    database connection

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,14 @@
 const express=require("express");
+const mongoose=require("mongoose");
 const app=express();
+require("dotenv").config();
 const port=8000;
+mongoose.connect(process.env.MONGO_URL )
+    .then(()=>console.log("DB Connection Successfull!"))
+    .catch((err)=>{
+        console.log(err);
+    });
+
 app.get("/",(req,res)=>{
     res.send("Hello world")
 });
